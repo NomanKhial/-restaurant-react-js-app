9c4fba8906ced2976f481d98e772f67eeda9ab8d
Noman Khial <240020@hu.edu.pk>
2025-07-25 20:36:44 +0000

    initi commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <header>
+      <img src={assets.logo} alt="" />
+    </header>
+  );
+}
+
+export default Navbar;
